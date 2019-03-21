23c66d6df04ba676fea4b961994c9ada2b60f9c5
semy0717 <mysyanovsergey@yandex.ru>
2019-03-21 18:15:25 +0400

    add project to repo

---
+++ src/main/java/com/collab/queuer/QueuerApplication.java
@@ -0,0 +1,13 @@
+package com.collab.queuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collab/queuer/QueuerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.collab.queuer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QueuerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
